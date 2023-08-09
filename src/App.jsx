a72ec5ac54ed2242394794bd27d695af09860c90
Pouya Barari Gelyard <93615101+pouyaBG@users.noreply.github.com>
2023-08-09 12:12:51 +0330

    cahneg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { ToastContainer } from 'react-toastify'
 import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
 import Signup from './components/Signup/Signup'
 function App() {
 
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Signup />
     </>
   )
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { validata } from "../validate/Validate";
-
+import styles from "../Signup/signup.module.css"
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
 const Signup = () => {
   const [errors, setErrors] = useState({})
+  const [toched, setToched] = useState({})
   const [data, setData] = useState({
     name: "",
     email: "",
@@ -14,7 +17,7 @@ const Signup = () => {
 
   useEffect(() => {
     setErrors(validata)
-  }, [data])
+  }, [data, toched])
 
   const changHandler = (event) => {
     if (event.target.name === "isAccepted") {
@@ -23,9 +26,36 @@ const Signup = () => {
       setData({ ...data, [event.target.name]: event.target.value });
     }
   };
+  const focusHandler = (e) => {
+    setToched({ ...toched, [e.target.name]: true })
+  }
+  const sumbitHandler = (e) => {
+    e.preventDefault()
+    if (!Object.keys(errors).length) {
+      console.log("ddd");
+      toast("ssdd", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    } else {
+      setToched({
+        name: true,
+        email: true,
+        password: true,
+        confirmPassword: true,
+        isAccepted: true,
+      })
+    }
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={sumbitHandler}>
         <h2>Signup</h2>
         <div>
           <label>Name</label>
@@ -34,16 +64,11 @@ const Signup = () => {
             name="name"
             value={data.name}
             onChange={changHandler}
+            onFocus={focusHandler}
           />
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="text"
-            name="email"
-            value={data.email}
-            onChange={changHandler}
-          />
+          <div className={styles.space}>
+            {errors.name && toched.name && <span className={styles.error}>{errors.name}</span>}
+          </div>
         </div>
         <div>
           <label>Email</label>
@@ -52,7 +77,11 @@ const Signup = () => {
             name="email"
             value={data.email}
             onChange={changHandler}
+            onFocus={focusHandler}
           />
+          <div className={styles.space}>
+            {errors.email && toched.email && <span className={styles.error}>{errors.email}</span>}
+          </div>
         </div>
         <div>
           <label>Password</label>
@@ -61,7 +90,11 @@ const Signup = () => {
             name="password"
             value={data.password}
             onChange={changHandler}
+            onFocus={focusHandler}
           />
+          <div className={styles.space}>
+            {errors.password && toched.password && <span className={styles.error}>{errors.password}</span>}
+          </div>
         </div>
         <div>
           <label>Confirm Password</label>
@@ -70,16 +103,24 @@ const Signup = () => {
             name="confirmPassword"
             value={data.confirmPassword}
             onChange={changHandler}
+            onFocus={focusHandler}
           />
+          <div className={styles.space}>
+            {errors.confirmPassword && toched.confirmPassword && <span className={styles.error}>{errors.confirmPassword}</span>}
+          </div>
         </div>
-        <div>
-          <label>i accepted terms of privacy policy</label>
+        <div style={{ display: "flex" }}>
           <input
             type="checkbox"
             name="isAccepted"
             value={data.isAccepted}
             onChange={changHandler}
+            onFocus={focusHandler}
           />
+          <label>i accepted terms of privacy policy</label>
+        </div>
+        <div className={styles.space}>
+          {errors.isAccepted && toched.isAccepted && <span className={styles.error}>{errors.isAccepted}</span>}
         </div>
         <div>
           <a href="#">Login</a>
